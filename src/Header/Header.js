ab49beddc6d19ebffb70f5e49a33928bca692b8a
Ildar <ildar00.ar@gmail.com>
2022-02-18 17:38:01 +0300

    Добавил ссылки для вкладок

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return(
         <div className="header">
-            <div className="item name">Отель Уют 3 звезды</div>
-            <div className="item book">Бронирование</div>
-            <div className="item delivery">Доставка</div>
-            <div className="item about">О нас</div>
+            <div> <NavLink to="/" className="item name">Отель Уют 3 звезды</NavLink> </div>
+            <div> <NavLink to="/book" className="item book">Бронирование</NavLink> </div>
+            <div> <NavLink to="/delivery" className="item delivery">Доставка</NavLink> </div>
+            <div> <NavLink to="/about" className="item about">О нас</NavLink> </div>
         </div>
     )
 }
